1f8e3e0cdde7b5cd44bc7af0457be5c661ec42b1
Anar Pashayev <anarpawayev@gmail.com>
2021-06-01 15:49:14 +0400

    firs commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+
+namespace LazyClass
+{
+    class Program
+    {
+        static void Count(object obj)
+        {
+            int x = (int)obj;
+            for (int i = 1; i < 9; i++, x++)
+            {
+                Console.WriteLine($"{x * i}");
+            }
+            Console.WriteLine("__________");
+
+        }
+        static void MyTime(object obj)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            int num = 0;
+            TimerCallback timerCallback = MyTime;//metod obratnogo vizova
+            Timer timer = new Timer(timerCallback, null, 0, 1000);
+            Console.ReadLine();
+        }
+    }
+}
